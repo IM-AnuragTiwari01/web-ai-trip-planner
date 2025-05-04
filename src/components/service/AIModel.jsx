098431aa5bd1a1fc008f69776c8a175b4429d28f
Anurag Tiwari <anurag.tiwari.en@gmail.com>
2025-05-04 20:41:41 +0530

    last commit

--- src/components/service/AIModel.jsx
+++ src/components/service/AIModel.jsx
@@ -1,13 +1,18 @@
-import { GoogleGenAI } from "@google/genai";
+// import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GOOGLE_GEMINI_API_KEY });
+// const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GOOGLE_GEMINI_API_KEY });
 
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
-    contents: "Explain how AI works in a few words",
-  });
-  console.log(response.text);
-}
+// async function main() {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.0-flash",
+//     contents: "Explain how AI works in a few words",
+//   });
+//   console.log(response.text);
+// }
 
-await main();
+// await main();
+//--------------------------------------
+
+
+
+// problem integating the LLM
