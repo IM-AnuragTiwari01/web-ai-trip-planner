afb33b0f0a30e833643e081a58996aab05cff85c
Anurag Tiwari <anurag.tiwari.en@gmail.com>
2025-10-20 21:23:22 +0530

    update apikeys

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -44,7 +44,7 @@ function CreateTrip() {
       .replace('{totalDays}', formData?.noOfDays);
 
     const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
     const chat = model.startChat();
     const result = await chat.sendMessage(FINAL_PROMPT);
     const response = await result.response;
